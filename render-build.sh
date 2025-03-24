f21de6110c125e1d265252bdd23c6be0e88b6db8
juanesflorezp <juanesflorezp@gmail.com>
2025-03-24 15:55:09 -0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
-echo "📦 Instalando Chromium..."
-apt-get update && apt-get install -y chromium
+echo "📦 Descargando Chromium..."
+mkdir -p /opt/render/project/chrome
+curl -fsSL https://github.com/macchrome/linux-snapshots/releases/download/v118.0.5993.70-r1161727/chrome-linux.zip -o chrome-linux.zip
+unzip chrome-linux.zip -d /opt/render/project/chrome
+mv /opt/render/project/chrome/chrome-linux /opt/render/project/chrome/chromium
 
 echo "📦 Instalando ChromeDriver..."
-CHROME_VERSION=$(curl -sL https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
-curl -fsSL "https://chromedriver.storage.googleapis.com/${CHROME_VERSION}/chromedriver_linux64.zip" -o chromedriver.zip
+CHROMEDRIVER_VERSION=$(curl -sL https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+curl -fsSL "https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip" -o chromedriver.zip
 unzip chromedriver.zip
 mv chromedriver /opt/render/project/chromedriver
 chmod +x /opt/render/project/chromedriver
