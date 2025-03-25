8e50ab14cb31c30ae2304f7137b0cfdba3cac3b2
juanesflorezp <juanesflorezp@gmail.com>
2025-03-24 20:40:34 -0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,8 +3,15 @@
 # Actualizar paquetes
 apt-get update 
 
-# Instalar Google Chrome
-apt-get install -y google-chrome-stable
+# Instalar dependencias necesarias
+apt-get install -y wget curl unzip
 
-# Instalar ChromeDriver
-apt-get install -y chromium-chromedriver
+# Descargar e instalar Google Chrome
+wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
+
+# Descargar e instalar ChromeDriver
+CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d'.' -f1)
+wget -q "https://chromedriver.storage.googleapis.com/$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION)/chromedriver_linux64.zip"
+unzip chromedriver_linux64.zip -d /usr/local/bin/
+chmod +x /usr/local/bin/chromedriver
