d0553ac43573c95b005223a53d905daa14728174
juanesflorezp <juanesflorezp@gmail.com>
2025-03-25 16:08:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import io
 import time
+import tempfile
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -53,13 +54,14 @@ async def procesar_archivo(file: UploadFile = File(...)):
         chrome_options.add_argument("--disable-dev-shm-usage")  
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--remote-debugging-port=9222")
-        chrome_options.add_argument("--user-data-dir=/tmp/chrome-profile")
-
-        # Eliminamos el modo headless para que se vea la interfaz gráfica
         
         # Ubicación de Chromium
         chromium_path = "/usr/bin/chromium"
         chrome_options.binary_location = chromium_path
+
+        # Generar un directorio temporal único para cada sesión de Chrome
+        temp_user_data_dir = tempfile.mkdtemp()
+        chrome_options.add_argument(f"--user-data-dir={temp_user_data_dir}")
 
         # Inicializar el WebDriver
         service = Service("/usr/bin/chromedriver")
