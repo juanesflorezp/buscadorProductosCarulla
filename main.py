1555abf4deccd273c28b1e61619a0f52bacf9c2c
juanesflorezp <juanesflorezp@gmail.com>
2025-04-01 14:50:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ async def procesar_archivo(file: UploadFile = File(...)):
         print(f"✅ ChromeDriver cargado correctamente desde: {chromedriver_path}")
 
         driver.get('https://www.carulla.com')
-        WebDriverWait(driver, 10).until(
+        WebDriverWait(driver, 11).until(
             EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/header/section/div/div[1]/div[2]/form/input'))
         )
 
@@ -97,7 +97,7 @@ async def procesar_archivo(file: UploadFile = File(...)):
             print(f"🔍 Buscando código de barras: {codigo_barras}")
 
             try:
-                search_field = WebDriverWait(driver, 10).until(
+                search_field = WebDriverWait(driver, 11).until(
                     EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/header/section/div/div[1]/div[2]/form/input'))
                 )
                 search_field.clear()
